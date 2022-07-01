2251e020fa9857fa00f8ce6e6786bb07df718513
atikis87 <ati.radeone@gmail.com>
2022-07-01 13:46:52 +0200

    add settings

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import Application from './src/Application.js';
+
+const app = new Application();
+
+app.run();
---
+++ app/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+    rootDir: 'tests',
+    verbose: true,
+    transform: {}
+};
+
+export default config;
